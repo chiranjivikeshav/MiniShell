1d71fcbaa22e091fcec3f130a187e839d9e6b5e3
chiranjivi <chiranjivikeshav1@gmail.com>
2026-02-18 13:40:37 +0530

    build: bind version with build ( -v or --version )

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include "shell.h"
+#include <iostream>
+#include <string>
 
-int main() {
+void print_version() {
+    std::cout << "minishell " << MINISHELL_VERSION << "\n";
+}
+
+int main(int argc, char* argv[]) {
+
+    if (argc > 1) {
+        std::string arg = argv[1];
+
+        if (arg == "-v" || arg == "--version") {
+            print_version();
+            return 0;
+        }
+
+        std::cerr << "Unknown option: " << arg << "\n";
+        return 1;
+    }
+
     Shell shell;
     shell.run();
     return 0;
